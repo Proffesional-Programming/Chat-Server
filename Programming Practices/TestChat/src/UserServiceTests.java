8a648dffde6356ea9146fde6cb4b12b290505911
chibao003 <49422190+chibao003@users.noreply.github.com>
2020-01-10 13:05:26 +0700

    Chat

---
+++ Programming Practices/TestChat/src/UserServiceTests.java
@@ -0,0 +1,22 @@
+import eiu.edu.vn.DataStore.DataStore;
+import eiu.edu.vn.Services.UserService;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.security.NoSuchAlgorithmException;
+
+public class UserServiceTests {
+    DataStore store = new DataStore();
+    UserService userService;
+
+    public UserServiceTests() {
+        this.userService = new UserService(this.store);
+    }
+
+    @Test
+    public void LoginTests() throws NoSuchAlgorithmException {
+        this.userService.createUser("bao", "123");
+        boolean test = this.userService.createUser("nam", "123");
+        Assert.assertTrue(test);
+    }
+}
--- Programming Practices/src/eiu/edu/vn/Main.java
+++ Programming Practices/src/eiu/edu/vn/Main.java
@@ -15,7 +15,6 @@ import java.util.UUID;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
     }
